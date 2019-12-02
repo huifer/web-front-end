57a92dc3d81e142874feeaf282eecbc9525533a9
huifer <huifer97@163.com>
2019-12-02 16:09:38 +0800

    函数前空格问题

--- vue/lesson0/my-project/.eslintrc.js
+++ vue/lesson0/my-project/.eslintrc.js
@@ -25,6 +25,7 @@ module.exports = {
     'generator-star-spacing': 'off',
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 函数前空格问题
     'space-before-function-paren':["error", {"anonymous": "always", "named": "never", "asyncArrow": "always"}]
    }
 }
